9c91698d17ee1d8593809da1a59d3d087802c915
WispyMouse <wispymouse@gmail.com>
2020-12-30 11:35:00 -0800

    Tile Library Searching
    Simple searching through the Tile Library for initialization.
    
    Very temporary, but works for now!

--- WeAllHaveDreams/Assets/ATOM/MapAssets/Tiles/TileLibrary.cs
+++ WeAllHaveDreams/Assets/ATOM/MapAssets/Tiles/TileLibrary.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TileLibrary : SingletonBase<TileLibrary>
 {
+    public GameplayTile[] Tiles;
+    Dictionary<string, GameplayTile> NamesToTiles { get; set; } = new Dictionary<string, GameplayTile>();
+
     public GameplayTile DefaultTile;
 
     public static GameplayTile GetTile(string tileName)
     {
-        return Singleton.DefaultTile;
+        if (Singleton.NamesToTiles.TryGetValue(tileName, out GameplayTile foundTile))
+        {
+            return foundTile;
+        }
+
+        GameplayTile matchingTile = Singleton.Tiles.FirstOrDefault(tile => tile.name == tileName);
+
+        if (matchingTile == null)
+        {
+            DebugTextLog.AddTextToLog($"Could not find a Tile in the Library with the name {tileName}. Returning a default.");
+            return Singleton.DefaultTile;
+        }
+
+        Singleton.NamesToTiles.Add(tileName, matchingTile);
+        return matchingTile;
     }
 }
